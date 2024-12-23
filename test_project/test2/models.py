d797767671ff000b66bd0f4255b21ec1b21fd96e
abhinav b <abhinavb4067@gmail.com>
2024-12-23 13:57:31 +0530

    employee added latest

--- test_project/test2/models.py
+++ test_project/test2/models.py
@@ -4,4 +4,5 @@ from test1.models import *
 # Create your models here.
 class Employee(models.Model):
     name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, null=True, blank=True)
     o_id =models.ForeignKey(Organization, on_delete=models.CASCADE, related_name="employees")
