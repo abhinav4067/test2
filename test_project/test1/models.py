df2ccf70134078236a71379ab961b0a9b42cb815
abhinav b <abhinavb4067@gmail.com>
2024-12-23 13:01:59 +0530

    organization_i3

--- test_project/test1/models.py
+++ test_project/test1/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 class Organization(models.Model):
     name = models.CharField(max_length=255)
-    place = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
